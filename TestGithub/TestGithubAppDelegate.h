8fe15868af5b78c732c877de6aa126255bb7ae2b
wattanadev <wattanadev@hotmail.com>
2011-09-03 10:24:53 +0700

    First Commit
    Window Based Template

---
+++ TestGithub/TestGithubAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TestGithubAppDelegate.h
+//  TestGithub
+//
+//  Created by wattana on 9/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestGithubAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
